78e08acc76cae957a1f99e76f6b9c2b6ecc506df
ojgup <48079091+ojgup@users.noreply.github.com>
2019-11-29 09:10:38 +1100

    Added the Computers Controller

---
+++ Challenge 3-b/Challenge 3-b/Controllers/ComputersController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Challenge_3_b.Models;
+using System.Data.SqlClient;
+
+namespace Challenge_3_b.Controllers
+{
+    public class ComputersController : ApiController
+    {
+        // GET: api/Computers
+        public IEnumerable<Computer> Get()
+        {
+            SqlConnection conn = new SqlConnection("Server=tcp:civapi.database.windows.net,1433;Initial Catalog=civapi;User ID=civ_user;Password=Monday1330;");
+
+            conn.Open();
+            SqlCommand getComputers = new SqlCommand("SELECT C.NUMBER, C.AssembledYear, ROOM.BUILDING, ROOM.ROOMNO, ROOM.CAPACITY " +
+                "FROM COMPUTER C INNER JOIN ROOM ON ROOM.ROOMNO = C.ROOMNO AND ROOM.BUILDING = C.BUILDING; ", conn);
+           
+            SqlDataReader datareader = getComputers.ExecuteReader();
+
+            List<Computer> listOfComputers = new List<Computer>();
+
+            while (datareader.Read())
+            {
+                listOfComputers.Add(new Computer(int.Parse(datareader[0].ToString()), int.Parse(datareader[1].ToString()), 
+                    new Room(datareader[2].ToString(), int.Parse(datareader[3].ToString()), int.Parse(datareader[4].ToString()) ) 
+                    ));
+
+            }
+
+            return listOfComputers;
+        }
+
+        // GET: api/Computers/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Computers
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Computers/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Computers/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Challenge 3-b/Challenge 3-b/Controllers/RoomsController.cs
+++ Challenge 3-b/Challenge 3-b/Controllers/RoomsController.cs
@@ -5,12 +5,13 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Data.SqlClient;
+using Challenge_3_b.Models;
 namespace Challenge_3_b.Controllers
 {
     public class RoomsController : ApiController
     {
         // GET: api/Rooms
-        public IEnumerable<string> Get()
+        public IEnumerable<Room> Get()
         {
             SqlConnection conn = new SqlConnection("Server=tcp:civapi.database.windows.net,1433;Initial Catalog=civapi;User ID=civ_user;Password=Monday1330;");
 
@@ -19,7 +20,15 @@ namespace Challenge_3_b.Controllers
 
             SqlDataReader datareader = getRooms.ExecuteReader();
 
-            return new string[] { "value1", "value2" };
+            List<Room> listOfRooms = new List<Room>();
+
+            while(datareader.Read())
+            {
+                listOfRooms.Add(new Room(datareader[0].ToString(), int.Parse(datareader[1].ToString()), int.Parse(datareader[2].ToString())));
+
+            }
+
+            return listOfRooms;
         }
 
         // GET: api/Rooms/5
